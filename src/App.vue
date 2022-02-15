e88839df0d7e9b74b06437f51580a79da00261ac
Stanley Seow <stanleyseow@gmail.com>
2022-02-15 16:57:34 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+}
+.red {
+  background: red;
+}
+</style>
